d9b9611ca8ac1cf39c83f3bb2424e23fd8800326
AsmaMostafatawfik <asmaa129xzz@gmail.com>
2025-07-23 06:59:56 +0300

    Fix: added default export to payments/page.tsx

--- app/admin/dashboard/payments/page.tsx
+++ app/admin/dashboard/payments/page.tsx
@@ -1,7 +1,9 @@
-export default function AdminsPage() {
+// Example fix: page.tsx must have a default export
+
+export default function PaymentsPage() {
   return (
     <div>
-      <h1>Admins Payment</h1>
+      <h1>Payments Dashboard</h1>
     </div>
   );
 }
---
+++ app/admin/dashboard/setting/page.tsx
@@ -0,0 +1,9 @@
+// Example fix: page.tsx must have a default export
+
+export default function PaymentsPage() {
+  return (
+    <div>
+      <h1>stting Dashboard</h1>
+    </div>
+  );
+}
